59ae749386ff3ddef99a64126c7bf5a712a950c8
DMarci2001 <91952341+DMarci2001@users.noreply.github.com>
2025-06-11 08:37:25 +0200

    Update spiderbot.js

--- spiderbot.js
+++ spiderbot.js
@@ -2100,7 +2100,7 @@ async function postServerGuide(channel) {
                     inline: false
                 },
                 {
-                    name: '✍️ Scriptorium',
+                    name: '🖋 Scriptorium',
                     value: `${channels.writingChat} - General discourse on the craft\n${channels.writingHelp} - Here, our community provides guidance in your literary questions\n${channels.onePageCritique} - Submit short excerpts for detailed feedback\n${channels.snippetShowcase} - Display your finest work for admiration`,
                     inline: false
                 },
